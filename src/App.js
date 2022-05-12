815f04b64ab1edceda2cc89e15c030473a6f408a
Saiket Das <saiketdas02@gmail.com>
2022-05-12 19:14:40 +0800

    Route add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home/Home/Home';
-import Appointment from './Pages/Private/Appointment/Appointment';
+import About from './Pages/About/AboutUs/AboutUs';
+import Login from './Pages/Login/Login/Login';
+import SignUp from './Pages/Login/SignUp/SignUp';
+import Appointment from './Pages/Private//Appointment/Appointment';
 import Header from './Pages/Shared/Header/Header';
 
 function App() {
@@ -14,7 +17,11 @@ function App() {
 
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
         <Route path='/appointment' element={<Appointment></Appointment>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/signup' element={<SignUp></SignUp>}></Route>
       </Routes>
     </div>
   );
