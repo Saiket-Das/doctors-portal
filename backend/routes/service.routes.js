8e2e2197fdc89c40f64e9de6bfd14d2e7d8873ce
Saiket Das <saiketdas02@gmail.com>
2023-03-23 23:37:24 +0600

    Backend - Service routes created

---
+++ backend/routes/service.routes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+// router.route("/");
+/** 
+        * @api {get} /user - All service
+        * @apiDescription Get all service 
+                
+        * @apiSucess {Object[]}  All the service
+        
+        // * @apiError {Unauthorization 401} Unauthorized      Only Authenticated Users can access the data. 
+        // * @apiError {Forbidden 403} Forbidden      Only Admin users can access the data
+     **/
+
+module.exports = router;
