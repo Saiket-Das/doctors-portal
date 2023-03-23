1971aabe725218ceacc153472c0af60b12e24e65
Saiket Das <saiketdas02@gmail.com>
2023-03-23 23:31:55 +0600

    Database connected with backend

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const dbConnection = require("./config/db");
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 8000;
+
+// ------> Middleware
+app.use(cors());
+app.use(express.json());
+
+dbConnection();
+
+// Routes
+
+app.get("/", (req, res) => {
+  res.send("Doctor's Portal is running");
+});
+
+app.listen(port, function () {
+  console.log(`Doctor's Portal is running on ${port}`.yellow.bold);
+});
