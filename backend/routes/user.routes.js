61790da9172bdef56ca676b29b8dc04b3be31336
Saiket Das <saiketdas02@gmail.com>
2023-03-24 22:34:30 +0600

    Backend - User routes created

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+// router.route("/").get(usersControllers.getUsers);
+
+// router.route("/:email").put(usersControllers.createUser);
+
+// router
+//   .route("/admin/:email")
+//   .get(usersControllers.getAdmin)
+//   .patch(usersControllers.createAdmin);
+
+module.exports = router;
