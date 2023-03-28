b06819ae3afe6fd53ad09f99f891bce51d3317a8
Saiket Das <saiketdas02@gmail.com>
2023-03-28 23:07:45 +0600

    Backend - Service controllers created

---
+++ backend/controllers/service.controllers.js
@@ -0,0 +1,15 @@
+exports.getServices = async (req, res) => {
+  try {
+    //  const User = await getServicessService();
+    res.status(200).json({
+      success: true,
+      data: User,
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      message: "What the issue wrong",
+      error: error.message,
+    });
+  }
+};
