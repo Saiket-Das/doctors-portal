87f88335c74103035d66cfcb10e35853751af705
Saiket Das <saiketdas02@gmail.com>
2023-03-28 23:14:46 +0600

    Backend - User controller created

---
+++ backend/controllers/user.controllers.js
@@ -0,0 +1,15 @@
+exports.getUsers = async (req, res) => {
+  try {
+    const User = await getUsersService();
+    res.status(200).json({
+      success: true,
+      data: User,
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      message: "Something went wrong",
+      error: error.message,
+    });
+  }
+};
