4710a72f038489c41b409a4cff6d8e52ff52bfdd
Saiket Das <saiketdas02@gmail.com>
2023-03-31 22:47:15 +0600

    Backend - Get admin api created

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -6,4 +6,6 @@ const usersControllers = require("../controllers/user.controllers");
 router.route("/").get(usersControllers.getUsers);
 router.route("/:email").put(usersControllers.createUser);
 
+router.route("/admin/:email").get(usersControllers.getAdmin);
+
 module.exports = router;
